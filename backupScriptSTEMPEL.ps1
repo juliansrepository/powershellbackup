ee2ed4183647600c42bf7691db233fbf9ea29433
Julian Alarcon <jalar015@fiu.edu>
2019-09-07 00:02:37 -0500

    Option to display current user directories was added.

--- backupScriptSTEMPEL.ps1
+++ backupScriptSTEMPEL.ps1
@@ -1,18 +1,14 @@
-$locations = @("Documents","Desktop","Downloads","Pictures") 
+$locations = @("Documents", "Desktop", "Downloads", "Pictures") 
 #Bookmarks for chrome, firefox, safari 
-$ListUsr=Read-Host "This script requires the username parameter, type yes or y to have them all displayed. Otherwise press any key"
-  if ($ListUsr -eq "yes" -OR $ListUsr -eq "y"){    
+$ListUsr = Read-Host "This script requires the username parameter, type yes or y to have them all displayed. Otherwise press any key"
+if ($ListUsr -eq "yes" -OR $ListUsr -eq "y") {    
     Get-ChildItem -Path c:\Users\
 }
-$UserName=Read-Host "Please type the username you wish to back up"
+$UserName = Read-Host "Please type the username you wish to back up"
 Write-Host "****** Starting backup script ******"
-foreach ($location in $locations){
-   if (!(Test-Path -path \\phitsrv.ad.fiu.edu\Backup\$UserName.$(get-date -f yyyy-MM-dd)\$location))  ## Check if destination exists, if no create folder
- {
- New-Item \\phitsrv.ad.fiu.edu\Backup\$UserName.$(get-date -f yyyy-MM-dd)\$location -type directory
- }
- robocopy $env:HOMEDRIVE\Users\$UserName\$location \\phitsrv.ad.fiu.edu\Backup\$UserName.$(get-date -f yyyy-MM-dd)\$location /S /R:3
+foreach ($location in $locations) {
+    if (!(Test-Path -path \\phitsrv.ad.fiu.edu\Backup\$UserName.$(get-date -f yyyy-MM-dd)\$location)) { ## Check if destination exists, if no create folder
+        New-Item \\phitsrv.ad.fiu.edu\Backup\$UserName.$(get-date -f yyyy-MM-dd)\$location -type directory
+    }
+    robocopy $env:HOMEDRIVE\Users\$UserName\$location \\phitsrv.ad.fiu.edu\Backup\$UserName.$(get-date -f yyyy-MM-dd)\$location /S /R:3
 }
-## make array with desktop,documents,downloads. Then iterate through it using a loop.
-
-##Todo: 1) create folder with time stamp 2) Bookmarks for chrome, firefox, safari
